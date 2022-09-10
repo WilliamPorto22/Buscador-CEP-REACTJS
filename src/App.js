f2696b6be2001144b6f4e7601702bd28b5cab842
WilliamPorto22 <williamporto0@gmail.com>
2022-09-10 19:35:04 -0300

    Atualizacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import {FiSearch} from 'react-icons/fi';
 import './styles.css';
 
 function App() {
+
+  const [input, setInput] = useState ('')
+
+  function handleSearch () {
+    alert ("VALOR DO INPUT  " + input )
+  }
+
   return (
     <div className="container">
       <h1 className="title"> Buscador CEP</h1>
@@ -10,9 +18,11 @@ function App() {
         <input
         type="text"
         placeholder="Digite seu CEP..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)  }
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size="25" color="#fff" />
         </button>
       </div>
